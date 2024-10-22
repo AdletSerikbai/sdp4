a4947a68c55b44f2009b689b6e976c112eac7cf1
AdletSerikbai <154731152+AdletSerikbai@users.noreply.github.com>
2024-10-22 15:56:49 +0500

    Add files via upload

---
+++ ChainOfResponsibilityPattern/Approver.java
@@ -0,0 +1,11 @@
+package ChainOfResponsibilityPattern;
+
+public abstract class Approver {
+    protected Approver nextApprover;
+
+    public void setNextApprover(Approver nextApprover) {
+        this.nextApprover = nextApprover;
+    }
+
+    public abstract void approveExpense(int amount);
+}
