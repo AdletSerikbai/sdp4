84967cc8c75fa60f7581c6e6e7f206cc7744fc68
AdletSerikbai <154731152+AdletSerikbai@users.noreply.github.com>
2024-10-22 15:59:12 +0500

    Add files via upload

---
+++ CommandPattern/LightOnCommand.java
@@ -0,0 +1,18 @@
+
+package CommandPattern;
+
+public class LightOnCommand implements Command {
+    private Light light;
+
+    public LightOnCommand(Light light) {
+        this.light = light;
+    }
+
+    public void execute() {
+        light.on();
+    }
+
+    public void undo() {
+        light.off();
+    }
+}
